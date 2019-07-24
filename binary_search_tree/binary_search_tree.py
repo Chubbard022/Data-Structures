dfa4fa2990fb64aadc61d9f660acf5bef90b9c7f
Chubbard022 <chubbard022@gmail.com>
2019-07-24 10:41:00 -0700

    start to binary Search Tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,18 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value:
+      #check to see if there is a left value
+      if not self.left:
+        self.left = BinarySearchTree(value)
+      else:
+        # recursively putting value in tree once find empty spot
+        self.left.insert(value)
+    else:
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
     pass
